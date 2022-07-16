2281722b3028d64558a81a2ae68c13836519318a
Victor Carballo <vicarballo03@gmail.com>
2022-07-16 15:35:46 -0600

    added the project

---
+++ pruebaNumeroUno/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace pruebaNumeroUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            do
+            {
+                Console.WriteLine("type a number between 1 and 1000 ");
+
+                try
+                {
+                    number = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            } while (!(number >= 1 && number <= 1000));
+
+            for(int i =1; i < number; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("foo bar");
+                }
+                else if (i % 3 == 0){
+                    Console.WriteLine("bar");
+                }
+
+                else if ((i % 5 == 0))
+                {
+                    Console.WriteLine("foo");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            pruebaDos dos = new pruebaDos();
+            Console.WriteLine("escribe la oracion que desea contar las vocales");
+            string oracion = Console.ReadLine();
+            dos.contar(oracion);
+            
+        }
+
+
+    }
+}
---
+++ pruebaNumeroUno/pruebaDos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaNumeroUno
+{
+    class pruebaDos
+    {
+       public void contar(string oracion) {
+            int a = 0;
+            int e = 0;
+            int i = 0;
+            int o = 0;
+            int u = 0;
+            oracion.ToLower();
+            for (int j =0; j < oracion.Length; j++)
+            {
+                string letra = oracion[j].ToString();
+                switch (letra)
+                {
+                    case "a":
+                        a += 1;
+                        break;
+                    case "e":
+                        e += 1;
+                        break;
+                    case "i":
+                        i += 1;
+                        break;
+                    case "o":
+                        o += 1;
+                        break;
+                    case "u":
+                        u += 1;
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("Cantidad de letras a " + a);
+            Console.WriteLine("Cantidad de letras e " + e);
+            Console.WriteLine("Cantidad de letras i " + i);
+            Console.WriteLine("Cantidad de letras o " + o);
+            Console.WriteLine("Cantidad de letras u " + u);
+        }
+    }
+}
